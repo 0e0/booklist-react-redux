e9b37b767d9e68a263b0d24745c797c17b98d072
0e0 <ejersonb@gmail.com>
2016-09-28 16:25:50 -0500

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+	return [
+		{ title: 'JavaScript: The Good Parts',
+			review: 'I really like this book!'
+		},
+		{ title: 'Harry Potter' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Eloquent Ruby' },
+	];
+}
